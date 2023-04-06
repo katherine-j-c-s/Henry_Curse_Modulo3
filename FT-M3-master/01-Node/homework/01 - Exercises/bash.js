af9f42b135423d0418c7cf47398ea61be6ae0db9
katherine-j-c-s <kathijcs@gmail.com>
2023-04-06 09:49:00 -0300

    finished node excersices

--- FT-M3-master/01-Node/homework/01 - Exercises/bash.js
+++ FT-M3-master/01-Node/homework/01 - Exercises/bash.js
@@ -2,7 +2,28 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write("prompt > ")
+   //le escribe este sting como data
+   process.stdin.on("data", (data) => {//frena a esperar que tenga una data
+      //espera a ver que tiene adentro
+      const args = data.toString().trim().split(" ")//lo convierte en string ,le quita los espacios del inicio y fin ,y lo convierte en array
+      const cmd = args.shift()//saca el primero y lo guarda aca
+      if (commands.hasOwnProperty(cmd)) {//si existe esta propiedad en commands
+         commands[cmd](print, args.join(" "));
+         //ejecutamos la funcion y por parametro
+         // le pasamos la funcion print y el resto de la data transformada en string de nuevo
+      }else {
+         print(`command not found: ${cmd}`)
+      }
+
+   })
+}
+
+function print(output) {//muestra en pantalla lo que necesitamos motras
+   process.stdout.write(output)
+   process.stdout.write("\nprompt > ")//vulve a vaciarce para proximo uso
+}
 
 bash();
 module.exports = {
--- FT-M3-master/01-Node/homework/01 - Exercises/commands/index.js
+++ FT-M3-master/01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,75 @@
-const fs = require("fs");
+const fs = require("fs");//core de node
 const utils = require("../utils/request");
 const process = require("process");
+const { error } = require("console");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+    //imprime en pantalla el directorio donde esta parado
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+    //imprime la fecha
+}
 
-function echo() {}
+function echo(print ,args) {
+    print(args)
+    //imprime el argumento con la data
+}
 
-function ls() {}
+function ls(print) {
+    //      ubicacion   fc que manera el error y la data
+    fs.readdir(".", function(error,files){
+        if(error) throw new Error(error)
+        print(files.join(" "))
+    })
+    //lee todos los archivos y carpetas que se encuentran en un directorio
+}
 
-function cat() {}
+function cat(print ,args) {
+    //para leer un archivo
+    //         ubicacion-buffer-fc que manera el error y la data
+    fs.readFile(args,'utf-8',(error,data)=>{
+        if(error) throw new Error(error)
+        print(data)
+    })
+    //lee que tiene dentro del archivo
+}
 
-function head() {}
+function head(print ,args) {
+    fs.readFile(args,'utf-8',(error,data)=>{
+        if(error) throw new Error(error)
+        const line = data.split("\n")[0].trim()
+        //separa la primera linea
+        print(line)
+    })
+    //muesta toda la info del head
+}
 
-function tail() {}
+function tail(print ,args) {
+    fs.readFile(args,'utf-8',(error,data)=>{
+        if(error) throw new Error(error)
+        const line = data.split("\n")
+        //separa la ultima linea
+        print(line[line.length-1].trim())
+    })
+    //muesta toda la info que esta de bajo del head
+}
 
-function curl() {}
+function curl(print ,args) {
+    utils.request(args, (error,response)=>{
+        if(error) return new Error(error)
+        print(response)
+    })
+    //es como una peticion
+}
 
-module.exports = {};
+module.exports = {pwd, date, curl, echo, ls, cat, head, tail};
+
+
+
+
+
+
+
